1919d07f84faa4857e9d4a595fb5e9bfd050e434
SanjaMilo <58039560+SanjaMilo@users.noreply.github.com>
2020-12-08 19:59:48 +0100

    Changes made on page Events Spase

--- js/eventsspace.js
+++ js/eventsspace.js
@@ -1,4 +1,4 @@
-// Function smooth scroll 
+// Function smooth scroll on button "Букирај не" click
 
 let btnScroll = document.querySelector('.btn-eve');
 let target= document.getElementById('host');
@@ -13,3 +13,29 @@ function scrollDown() {
 };
 
 btnScroll.addEventListener('click', scrollDown);
+
+// Opening Modal for viewing large image 
+
+let modalImg = document.getElementById("modalImage");
+let enlargedImg = document.getElementById("imgShow");
+let captionText = document.getElementById("caption");
+let closeX = document.querySelector(".close-x");
+
+let imagesOpeningModals = Array.from(document.querySelectorAll(".img-open-modal"));
+
+// Handler functions (callbacks)
+
+function openModalImage() {
+    modalImg.style.display = "block";
+    enlargedImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+
+function closeModalImage() {
+    modalImg.style.display = "none";
+}
+
+// Event listeners 
+imagesOpeningModals.forEach(imag => {imag.addEventListener("click", openModalImage)});
+closeX.addEventListener("click", closeModalImage);
